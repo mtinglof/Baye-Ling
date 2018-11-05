05a9118c229f2a4badc5d0cb39f79ee4344836a5
mtinglof <42756027+mtinglof@users.noreply.github.com>
2018-11-05 00:10:05 -0800

    Almost finished!

--- processor.py
+++ processor.py
@@ -6,12 +6,16 @@ class processor:
 
     def __init__(self): 
         self.callwords = ['you',  'bitch', 'ur', 'your', 'fucking', 'fuck', 'u', 'stop', 'asshole', 'crusty', 'roommate', 'hate', 'cares', 'left', 'shoutout', 'shut', 'trust', 'need', 'dear', 'stupid', 'aztec', 'cunt', 'dumbass', 'needs', 'sincerely', 'sorority', 'yourself', 'bunk', 'stealing', 'stole']
-        self.paths = [r'C:\Users\mting\Documents\Baye Ling\Baye-Ling\Callout.txt', 
-         r'C:\Users\mting\Documents\Baye Ling\Baye-Ling\personals.txt', 
-         r'C:\Users\mting\Documents\Baye Ling\Baye-Ling\meme.txt', 
+        self.seekwords = ['cute', 'issue', 'want', 'hmu', 'someone', 'pipe', 'where', 'tryna', 'relationship', 'dick', 'chill', 'buddy', 'respond', 'twink', 'looking', 'hot', 'suck', 'guy', 'girl', 'anyone', 'm', 'talk', 'gmail', 'number', 'orgasms', 'hell', 'session', 'smash', 'fine', 'down']
+        self.wordscount = 60
+        self.callcount = 0
+        self.seekcount = 0
+        self.paths = [r'C:\Users\mting\Documents\Baye Ling\Baye-Ling\Callout.txt',   
          r'C:\Users\mting\Documents\Baye Ling\Baye-Ling\seeking.txt']
         self.tokens = {}
-        self.titles = ("callout", "personals", "meme", "seeking")
+        self.calldic = {}
+        self.seekdic = {}
+        self.titles = ("callout", "seeking")
 
     def token(self): 
         for index, path in enumerate(self.paths):
@@ -20,17 +24,43 @@ class processor:
 
     def freq(self): 
         self.token()
-        pos = {}
         calldist = self.tokens['callout']
-        personaldist = self.tokens['personals']
-        memedist = self.tokens['meme']
         seekingdist = self.tokens['seeking']
         for word in self.callwords: 
-            pos[word] = list((calldist[word], (personaldist[word]+memedist[word]+seekingdist[word])))
-        return pos 
+            self.calldic[word] = calldist[word]
+            self.seekdic[word] = seekingdist[word]
+            self.callcount = calldist[word] + self.callcount
+        for word in self.seekwords: 
+            self.seekdic[word] = seekingdist[word] 
+            self.calldic[word] = calldist[word]
+            self.seekcount = seekingdist[word] + self.seekcount
+
+    def prob(self, submit): 
+        for word in submit:
+            subtoken = word_tokenize(submit)
+        samewords = []
+        seekwordtotal = self.seekcount + self.wordscount
+        callwordtotal = self.callcount + self.wordscount
+        for word in subtoken: 
+            for keyword in self.callwords: 
+                if word.lower() == keyword: 
+                    samewords.append(word.lower())
+            for keyword in self.seekwords: 
+                if word.lower() == keyword: 
+                    samewords.append(word.lower())
+        probseek = 96/400
+        probcall = 193/400
+        for word in samewords: 
+            probseek = ((self.seekdic[word]+1)/seekwordtotal)*probseek
+            probcall = ((self.calldic[word]+1)/callwordtotal)*probcall
+        percall = probcall/(probcall+probseek)
+        perseek = probseek/(probseek+probseek)
+        perpersonal = 1 - (percall*perseek)
+        print(perpersonal)
+        print("Probablity of a call out: " + str(percall) + "\nProbablity of a seeking request: " + str(perseek))
+
 
            
 test = processor()
 items = test.freq()
-for item in items: 
-    print(str(item) + ' ' + str(items[item]))
+test.prob("")
